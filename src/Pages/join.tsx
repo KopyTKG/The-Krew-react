740d7e2107ecdf032b40f7ca50b9576048b17e5c
Kopy_TKG <47574331+KopyTKG@users.noreply.github.com>
2021-04-13 14:28:10 +0200

    Update 0.1.3 (CSS rework -> style.css)

--- src/Pages/join.tsx
+++ src/Pages/join.tsx
@@ -3,12 +3,9 @@ import { Row, Col, Container } from 'reactstrap';
 import Title from '../Components/Subcomps/title';
 
 const App: React.FC = () => {
-    let h = 0;
-    if(window.innerHeight > 800) h = 788;
-    else h = 400;
     return(
         <>
-        <Container className=" bg-white-transparent margin-top inner" style={{height: h, overflowY: "hidden"}}>
+        <Container className="inner bg-white-transparent margin-top">
             <Row xs="12">
                 <Col xs="12" className="center-line">
                     <Title title="Join"/>
